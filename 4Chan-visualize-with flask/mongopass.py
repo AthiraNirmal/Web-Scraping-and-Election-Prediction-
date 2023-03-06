f25c2ea4539074956129dbd177bdb4df46c8bdf9
Athira Nirmal <63495996+AthiraNirmal@users.noreply.github.com>
2023-03-05 19:58:21 -0500

    Create mongopass.py

---
+++ 4Chan-visualize-with flask/mongopass.py
@@ -0,0 +1,1 @@
+mongopass= "mongodb://SocialMedia:Social95@ac-cm3ylq6-shard-00-00.7auqdfd.mongodb.net:27017,ac-cm3ylq6-shard-00-01.7auqdfd.mongodb.net:27017,ac-cm3ylq6-shard-00-02.7auqdfd.mongodb.net:27017/?ssl=true&replicaSet=atlas-o0qigq-shard-0&authSource=admin&retryWrites=true&w=majority"
